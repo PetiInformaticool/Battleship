2b224b2b1099e93779b52f485162d47f71a7ea2a
pety <alex.peticaru@gmail.com>
2021-05-28 23:04:09 +0300

    player modified

--- src/player2.cpp
+++ src/player2.cpp
@@ -5,12 +5,65 @@
 
 using namespace std;
 
+bool vecin(int i, int j, char b[][WIDTH]) {
+	for (int k = 0; k < 4; ++k) {
+		int _i = i + dl[k];
+		int _j = j + dc[k];
+		if (_i < 0 || _i >= LENGTH || _j < 0 || _j >= WIDTH) 
+			continue;
+		if (b[_i][_j] >= '0' && b[_i][_j] < '9')
+			return 1;	
+	}
+	return 0;
+}
+
+pair < int, int > partial(char b[][WIDTH]) {
+	for (int i = 0; i < LENGTH; ++i) {
+		for (int j = 0; j < WIDTH; ++j) {
+			if (b[i][j] == HIT) {
+				int vecini(0), orientare(-1);
+				for (int k = 0; k < 4; ++k) {
+					int _i = i + dl[k];
+					int _j = j + dc[k];
+					if (_i < 0 || _i >= LENGTH || _j < 0 || _j >= WIDTH) 
+						continue;
+					if (b[_i][_j] == HIT)
+						++vecini, orientare = (k > 1);
+				}
+				if (vecini > 1)
+					continue;
+				if (orientare + 1) {
+					for (int k = orientare * 2; k < (1 + orientare) * 2; ++k) {
+						int _i = i + dl[k];
+						int _j = j + dc[k];
+						if (_i < 0 || _i >= LENGTH || _j < 0 || _j >= WIDTH) 
+							continue;
+						if (b[_i][_j] == FREE) 
+							return {_i, _j};
+					}
+				}
+				else {
+					for (int k = 0; k < 4; ++k) {
+						int _i = i + dl[k];
+						int _j = j + dc[k];
+						if (_i < 0 || _i >= LENGTH || _j < 0 || _j >= WIDTH) 
+							continue;
+						if (b[_i][_j] == FREE) 
+							return {_i, _j};
+					}
+				}
+			}
+		}
+	}
+	return {-1, -1};
+}
+
 int main () {
 	srand(time(NULL));
 	FILE* pipe[2];
-  pipe[0] = fopen("pipes/server0", "rb");
-  pipe[1] = fopen("pipes/player0", "wb");
-  board b;
+	pipe[0] = fopen("pipes/server0", "rb");
+	pipe[1] = fopen("pipes/player0", "wb");
+	board b;
 	int ships[]= {0, 0, 3, 2, 1, 1};
 	while (1) {
 		b = getBoard(pipe[0]);
@@ -19,9 +72,9 @@ int main () {
 		}
 		int shiplen, correct;
 		do {
-		  shiplen = rand() % 4 + 2;
-	  }while(!ships[shiplen]);
-	  do {
+			shiplen = rand() % 4 + 2;
+		}while(!ships[shiplen]);
+		do {
 			int x = rand() % LENGTH;
 			int y = rand() % WIDTH;
 			int dir = rand() % 4;
@@ -34,17 +87,21 @@ int main () {
 			}
  		}while (!correct);
 	}
-	
   while (1) {
 		b = getBoard(pipe[0]);
-		if (b.state == 1) {
+		if (b.state == 1) 
 		  break;
+		
+		int x, y;	
+		pair < int, int > part = partial(b.board);
+		if (part.first == -1) {
+			do {
+				y = rand() % WIDTH;
+				x = rand() % LENGTH;
+			}while (b.board[x][y] != FREE || ((x + y)&1) == 0 || vecin(x, y, b.board));
 		}
-		 int x, y;
-		do {
-			y = rand() % WIDTH;
-			x = rand() % LENGTH;
-		}while (b.board[x][y] != FREE);
+		else 
+			x = part.first, y = part.second;
 	  Player2 lmao = {x, y};
 	  fwrite(&lmao, sizeof(Player2), 1, pipe[1]);
 	  fflush(pipe[1]);
